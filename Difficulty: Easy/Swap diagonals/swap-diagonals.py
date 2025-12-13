de227d32a2d1eb0804b59bf02b58058d18b89f20
KARRI HARIKA REDDY <harikareddykarri@gmail.com>
2025-12-13 17:51:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap diagonals/swap-diagonals.py
@@ -0,0 +1,8 @@
+class Solution:
+    def swapDiagonal(self, mat):
+        n = len(mat)
+        for i in range(n):
+            mat[i][i], mat[i][n - i - 1] = mat[i][n - i - 1], mat[i][i]
+        return mat
+
+
