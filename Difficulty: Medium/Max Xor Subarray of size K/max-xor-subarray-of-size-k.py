aab59ab6ee2370a83f4d8945b61a5ece838ed8bf
KARRI HARIKA REDDY <harikareddykarri@gmail.com>
2026-01-06 09:44:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Xor Subarray of size K/max-xor-subarray-of-size-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubarrayXOR(self, arr, k):
+        n = len(arr)
+
+        curr_xor = 0
+        for i in range(k):
+            curr_xor ^= arr[i]
+
+        max_xor = curr_xor
+
+        for i in range(k, n):
+            curr_xor ^= arr[i]      # add new element
+            curr_xor ^= arr[i - k]  # remove element leaving window
+            max_xor = max(max_xor, curr_xor)
+
+        return max_xor
+
+        
+       
