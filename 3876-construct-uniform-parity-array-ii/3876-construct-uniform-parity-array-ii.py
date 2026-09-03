03a6d4004c7915d350e73cec2d9656d3d193e13a
KARRI HARIKA REDDY <harikareddykarri@gmail.com>
2026-09-03 23:22:44 +0530

    Time: 11 ms (90.85%), Space: 35.3 MB (73.17%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def uniformArray(self, nums1: List[int]) -> bool:
+
+        mn = min(nums1)
+
+        if mn % 2 == 1:
+            return True
+
+        for x in nums1:
+            if x % 2 == 1:
+                return False
+
+        return True
